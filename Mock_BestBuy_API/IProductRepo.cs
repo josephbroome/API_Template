9f66f1d64692fc3fc8f8c6a52c8e4d7bc1326f32
Joseph Broome <josephlloydbroome@gmail.com>
2022-08-07 15:42:03 -0700

    updates

---
+++ Mock_BestBuy_API/IProductRepo.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+
+namespace Mock_BestBuy_API
+{
+    public interface IProductRepo
+    {
+        public IEnumerable<Product> GetProducts();
+        public Product GetProduct(int id);
+        public void InsertProduct(Product prod);
+        public void UpdateProduct(Product prod);
+        public void DeleteProduct(Product prod);
+
+
+
+    }
+}
--- Mock_BestBuy_API/Startup.cs
+++ Mock_BestBuy_API/Startup.cs
@@ -7,8 +7,10 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
+using MySql.Data.MySqlClient;
 using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -31,6 +33,22 @@ namespace Mock_BestBuy_API
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "Mock Best Buy API", Version = "v1" });
             });
+
+            services.AddScoped<IDbConnection>(s =>
+            {
+                IDbConnection conn = new MySqlConnection(Configuration.GetConnectionString("bestbuy"));
+                conn.Open();
+                return conn;
+            });
+
+            services.AddTransient<IProductRepo, ProductRepo>();
+
+            services.AddCors(options =>                         // CORS = Cross Open Resource Sharing   
+            {                                                   // Opens up the Api to not just limit the callers to a specific domain or individual URL
+                options.AddPolicy("AllowOrigin", builder => builder.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader());
+            });
+
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -46,6 +64,8 @@ namespace Mock_BestBuy_API
             app.UseHttpsRedirection();
 
             app.UseRouting();
+
+            app.UseCors("AllowedOrigin");    //Added this in order to open up CORS
 
             app.UseAuthorization();
 
